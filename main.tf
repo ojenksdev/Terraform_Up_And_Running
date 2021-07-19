61bb8ba7205d528dd6a5ef41e19aa2e58b32c670
ojenksdev <ojenksdev@gmail.com>
2021-07-19 10:52:31 -0500

    added ASG config, variables, etc.

--- main.tf
+++ main.tf
@@ -2,30 +2,65 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
+variable "server_port" {
+  description = "The port the server will use for HTTP requests."
+  type = number
+  default = 8080
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_launch_configuration" "example" {
     ami = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
 
     user_data = <<-EOF
         #!/bin/bash
         echo "Hello World!" > index.html
-        nohup busybox httpd -f -p 8080
+        nohup busybox httpd -f -p ${var.server_port} &
         EOF
 
-    tags = {
-        Name = "terraform-example"
-    }   
+    # Required when using a launch config with an ASG
+    lifecycle {
+      create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+      key = "Name"
+      value = "terraform-asg-example"
+      propagate_at_launch = true
+    }
 }
 
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
 
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = var.server_port
+        to_port = var.server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP for the web server."
 }
 
